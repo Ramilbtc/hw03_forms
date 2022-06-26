9887eb7143d326edc480b11065a5f38cdeee7579
Рамиль Абзалов <ramadjamal@yandex.ru>
2022-06-26 13:10:33 +0300

    4

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,60 +1,64 @@
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.contrib.auth.views import (LoginView,
+                                       LogoutView,
+                                       PasswordChangeView,
+                                       PasswordChangeDoneView,
+                                       PasswordResetView,
+                                       PasswordResetDoneView,
+                                       PasswordResetConfirmView,
+                                       PasswordResetCompleteView)
 from django.urls import path
 from . import views
 
 app_name = 'users'
 
 
-# Class Based View это view на основе классов, поэтому с ними используется .as_view()
 urlpatterns = [
-    # регистрация
     path('signup/', views.SignUp.as_view(), name='signup'),
-    # авторизация
     path(
         'login/',
         LoginView.as_view(template_name='users/login.html'),
         name='login',
     ),
-    # выход из аккаунта и показ прощальной страницы
     path(
         'logout/',
-        LogoutView.as_view(template_name='users/logged_out.html'),
+        LogoutView
+        .as_view(template_name='users/logged_out.html'),
         name='logout',
     ),
-    # смена пароля
     path(
         'password_change/',
-        PasswordChangeView.as_view(template_name='users/password_change_form.html'),
+        PasswordChangeView
+        .as_view(template_name='users/password_change_form.html'),
         name='password_change_form',
     ),
-    # сообщение об успешном изменении пароля
     path(
         'password_change/done/',
-        PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
+        PasswordChangeDoneView
+        .as_view(template_name='users/password_change_done.html'),
         name='password_change_done',
     ),
-    # восстановление/сброс пароля
     path(
         'password_reset/',
-        PasswordResetView.as_view(template_name='users/password_reset_form.html'),
+        PasswordResetView
+        .as_view(template_name='users/password_reset_form.html'),
         name='password_reset_form',
     ),
-    # сообщение об отправке ссылки на восстановление пароля
     path(
         'password_reset/done/',
-        PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+        PasswordResetDoneView
+        .as_view(template_name='users/password_reset_done.html'),
         name='password_reset_done',
     ),
-    # вход по ссылке для восстановления пароля
     path(
         'reset/<uidb64>/<token>/',
-        PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+        PasswordResetConfirmView
+        .as_view(template_name='users/password_reset_confirm.html'),
         name='password_reset_confirm',
     ),
-    # успешная смена пароля
     path(
         'reset/done/',
-        PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+        PasswordResetCompleteView
+        .as_view(template_name='users/password_reset_complete.html'),
         name='password_reset_complete',
     ),
 
--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,12 +1,9 @@
-# импортируем CreateView чтобы создать ему наследника
 from django.views.generic import CreateView
-# функция reverse_lazy позволяет получить URL по параметрам path()
 from django.urls import reverse_lazy
-# испортируем класс-формы, чобы сослаться на него во view-классе
 from .forms import CreationForm
 
 
 class SignUp(CreateView):
-    form_class = CreationForm   # с какой формой будет работать этот View-класс
-    success_url = reverse_lazy('posts:index')  # куда переадресовать пользователя после успешной отправки формы регистрации
-    template_name = 'users/signup.html'  # имя шаблона, куда будет передана переменная form с объектом html-формы
+    form_class = CreationForm
+    success_url = reverse_lazy('posts:index')
+    template_name = 'users/signup.html'
